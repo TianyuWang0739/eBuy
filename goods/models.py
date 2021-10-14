e8fb71fb6a887a0e4764f0b7021e0346b9dae446
TianyuWang0739 <87952211+TianyuWang0739@users.noreply.github.com>
2021-10-14 23:01:45 +0100

    Update models.py

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Create the goods category data list here.
+class GoodsCategory(models.Model):
+   # The name of categorary
+   gc_name=models.CharField(max_length=50)
+   # The sample of categorary
+   gc_sam=models.CharField(max_length=50)
+   # The picture of categorary
+   gc_pic=models.ImageField(upload_to='categorary')
+
+## Create the goods information data list here.
+class GoodsInfo(models.Model):
+   # The name of goods
+   gi_name=models.CharField(max_length=50)
+   # The proce of goods
+   gi_price=models.IntegerField(default=0)
+   # The description of goods
+   gi_des=models.CharField(max_length=3000)
+   # The picture of goods
+   gi_pic=models.ImageField(upload_to='goods')
+   # The category of goods
+   gi_cag=models.ForeignKey('GoodsCategory')
