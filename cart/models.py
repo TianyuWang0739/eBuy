1344e58e082c2773e18e216e607dd9e3dd2281e0
TianyuWang0739 <87952211+TianyuWang0739@users.noreply.github.com>
2021-10-21 01:14:08 +0100

    1

--- cart/models.py
+++ cart/models.py
@@ -1,35 +1,37 @@
 from django.db import models
 
+
 # Create the order information model here.
 class OrderInfo(models.Model):
     # Kinds of order status
-    status=(
-        (1,'Pending payment'),
-        (2,'Pending delivery'),
-        (3,'Pending receipt'),
-        (4,'Order completed'),
+    status = (
+        (1, 'Pending payment'),
+        (2, 'Pending delivery'),
+        (3, 'Pending receipt'),
+        (4, 'Order completed'),
     )
-    
+
     # Order Number
-    order_id=models.CharField(max_length=50)
+    order_id = models.CharField(max_length=50)
     # Receiving address
-    order_addr=models.CharField(max_length=100)
+    order_addr = models.CharField(max_length=100)
     # Recipient
-    order_reci=models.CharField(max_length=50)
+    order_reci = models.CharField(max_length=50)
     # Contact number
-    order_phon=models.CharField(max_length=20)
+    order_phon = models.CharField(max_length=20)
     # Shipping cost
-    order_ship=models.IntegerField(5)
+    order_ship = models.IntegerField(5)
     # Order extra information
-    order_extr=models.CharField(max_length=300)
+    order_extr = models.CharField(max_length=300)
     # Order status
-    order_stat=models.IntegerField(default=1,choices=status)
+    order_stat = models.IntegerField(default=1, choices=status)
+
 
 # Create the order goods model
 class OrderGoods(models.Model):
     # Product Information
-    goods_info=models.ForeignKey('goods.GoodsInfo',on_delete=models.CASCADE)
+    goods_info = models.ForeignKey('goods.GoodsInfo', on_delete=models.CASCADE)
     # Number of items
-    goods_numb=models.IntegerField()
+    goods_numb = models.IntegerField()
     # Product order
-    goods_order=models.ForeignKey('OrderInfo',on_delete=models.CASCADE)
+    goods_order = models.ForeignKey('OrderInfo', on_delete=models.CASCADE)
--- goods/models.py
+++ goods/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 
+
 # Create the goods category model here.
 class GoodsCategory(models.Model):
-   # The name of categorary
-   gc_name=models.CharField(max_length=50)
-   # The sample of categorary
-   gc_sam=models.CharField(max_length=50)
-   # The picture of categorary
-   gc_pic=models.ImageField(upload_to='categorary')
+    # The name of category
+    gc_name = models.CharField(max_length=50)
+    # The sample of category
+    # gc_sam = models.CharField(max_length=50)
+    # The picture of category
+    gc_pic = models.ImageField(upload_to='cag')
+
 
 # Create the goods information model here.
 class GoodsInfo(models.Model):
-   # The name of goods
-   gi_name=models.CharField(max_length=50)
-   # The proce of goods
-   gi_price=models.IntegerField(default=0)
-   # The description of goods
-   gi_des=models.CharField(max_length=3000)
-   # The picture of goods
-   gi_pic=models.ImageField(upload_to='goods')
-   # The category of goods
-   gi_cag=models.ForeignKey('GoodsCategory',on_delete=models.CASCADE)
+    # The name of goods
+    gi_name = models.CharField(max_length=50)
+    # The price of goods
+    gi_price = models.IntegerField(default=0)
+    # The description of goods
+    gi_des = models.CharField(max_length=3000)
+    # The picture of goods
+    gi_pic = models.ImageField(upload_to='goods')
+    # The category of goods
+    gi_cag = models.ForeignKey('GoodsCategory', on_delete=models.CASCADE)
+
